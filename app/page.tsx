2c744a1a8e43d35a0b94c91e96666614a451c5a1
Migulev <Migulev@yandex.ru>
2023-08-13 18:07:06 -0400

    +

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,9 @@
 import PokemonGrid from '@/components/pokemon-grid';
 import { getPokemonList } from '@/lib/pokemonAPI';
 
-// image fill
-// adaptive
-// add refresh button if its error
-// eslint
+// 2 languages
 // theme error
-// favicon in Next.js
-// fix pokemon types everywhere
-// suspense / loading
+// README.md
 
 export default async function Home() {
   const pokemonList = await getPokemonList();
