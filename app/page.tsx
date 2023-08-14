faaa2388dc23b75ddb2d325a37483a77234601df
Migulev <Migulev@yandex.ru>
2023-08-14 13:37:00 -0400

    +

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,6 @@
 import PokemonGrid from '@/components/pokemon-grid';
 import { getPokemonList } from '@/lib/pokemonAPI';
 
-// 2 languages
-// theme error
-// README.md
-
 export default async function Home() {
   const pokemonList = await getPokemonList();
 
